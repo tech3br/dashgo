1be15157091135cedd1be97e4d84fcb6b1cb38db
Daniel Filgueira <dfsdireito95@gmail.com>
2021-07-21 21:12:16 -0300

    feat: :sparkles: adding login and dashboard pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,28 @@
+import { Flex, Button, Stack, FormLabel, FormControl } from "@chakra-ui/react";
+
+import { Input } from "../components/Form/Input";
+
 export default function Home() {
-  return <h1>DashGo</h1>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Password" />
+        </Stack>
+
+        <Button as="a" type="submit" mt="6" colorScheme="pink" size="lg" href="/dashboard">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
