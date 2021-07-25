977124b099b70dd5650ddc85b49942fdb4e2d5a6
Daniel Filgueira <dfsdireito95@gmail.com>
2021-07-24 23:04:40 -0300

    :sparkles: Adding Structure to UserList Page
    initializing userlist page

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import { Header } from "../../components/Header";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+    </Box>
+  );
+}
